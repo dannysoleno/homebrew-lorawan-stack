f6eb50bbab64226a3e8f0179abef31456a8eb0a1
tti-ci <stack@thethingsindustries.com>
2020-02-27 18:22:18 +0100

    Brew formula update for lorawan-stack version v3.6.0

--- tti-lw-cli.rb
+++ tti-lw-cli.rb
@@ -2,24 +2,24 @@
 class TtiLwCli < Formula
   desc "CLI of The Things Stack for LoRaWAN"
   homepage "https://www.thethingsindustries.com"
-  version "3.5.3"
+  version "3.6.0"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.5.3/lorawan-stack-cli_3.5.3_darwin_amd64.tar.gz"
-    sha256 "83403ca3737ae6857ee15b8b3f441d1fb50be90058b84e19fa49b744ac6e29d3"
+    url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.6.0/lorawan-stack-cli_3.6.0_darwin_amd64.tar.gz"
+    sha256 "37b867a64a18799ce24231205d5335b99594fe0d9949479b0c927702f8f92a46"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.5.3/lorawan-stack-cli_3.5.3_linux_amd64.tar.gz"
-      sha256 "abb325a650b1d746ddbd267ce64b4388e9d9277f42c8a06142387e8d2a0b0765"
+      url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.6.0/lorawan-stack-cli_3.6.0_linux_amd64.tar.gz"
+      sha256 "fea5a8e76b990a22a29aef01620eba48864e6f8d80fa79765cb4ad56857a6273"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.5.3/lorawan-stack-cli_3.5.3_linux_arm64.tar.gz"
-        sha256 "576e35ef4819f073130c5452c1e0923cd61aa4ac7d20f9bad0323efd63349bc4"
+        url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.6.0/lorawan-stack-cli_3.6.0_linux_arm64.tar.gz"
+        sha256 "3b3699667f37bfe7b6ec5185226c076f6f5b21cbb5d2970cdaa5534f22eac88e"
       else
-        url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.5.3/lorawan-stack-cli_3.5.3_linux_armv6.tar.gz"
-        sha256 "26d632807443e5fe079b106d64d8cdf5f514d24b8059443463e613f100d3343e"
+        url "https://github.com/TheThingsIndustries/lorawan-stack/releases/download/v3.6.0/lorawan-stack-cli_3.6.0_linux_armv6.tar.gz"
+        sha256 "3c2e8ce87daac8557e7577e4ea561a96a31e2609efc629b800a95d2088c7b740"
       end
     end
   end
